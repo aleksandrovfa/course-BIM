2713a9d9e9c1601c243d795c34f7bec40f2e6c53
Федор Александров <fedfed95@yandex.ru>
2021-11-20 20:42:50 +0300

    Решение задач 7 занятия

---
+++ 7_Task/Part_1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a = Input(1); //Запись в массив сторон треугольника
+            double sa = GetArea(a); //Вычисление площади треугольника
+
+
+            double[] b = Input(2);
+            double sb = GetArea(b);
+
+            Console.WriteLine("Площадь первого = {0,2}", sa.ToString("0.00"));
+            Console.WriteLine("Площадь второго = {0}", sb.ToString("0.00"));
+
+            if (sa>sb)
+            {
+                Console.WriteLine("Площадь первого больше");
+            }
+            else if (sb>sa)
+            {
+                Console.WriteLine("Площадь второго больше");
+            }
+            else
+            {
+                Console.WriteLine("Площади равны");
+            }
+            Console.ReadKey();
+
+
+        } 
+        static double[] Input (int num)
+        {
+            Console.WriteLine("Задайте {0} треугольник длинами сторон через пробел", num);
+            string side = Console.ReadLine();
+            string[] sideArrayString = side.Split();
+            double[] sideArrayInt = new double[sideArrayString.Length];
+            for (int i = 0; i < sideArrayString.Length; i++)
+            {
+                sideArrayInt[i] = Convert.ToDouble(sideArrayString[i]);
+            }
+            return sideArrayInt;
+        }
+        static double GetArea (double[] side)
+        {
+            //Проверка на существование треугольника.
+            if (side[0] + side[1] > side[2] && side[0] + side[2] > side[1] && side[2] + side[1] > side[0])
+            {
+                double poluperimeter = (side[0] + side[1] + side[2]) / 2;
+                double s = Math.Sqrt(poluperimeter * (poluperimeter - side[0]) * (poluperimeter - side[1]) * (poluperimeter - side[2]));
+                return s;
+            }
+            else
+            {
+                Console.WriteLine("Такого треугольника не существует, площадь записана как 0");
+                return 0;
+            }
+        } 
+    }
+}
---
+++ 7_Task/Part_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны куба");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s, v;
+            GetSandV(n, out s, out v);
+            Console.WriteLine("Площадь поверхности куба = {0}", s);
+            Console.WriteLine("Объем куба = {0}", v);
+            Console.ReadKey();
+
+
+        }
+        static void GetSandV (int n, out int s, out int v)
+        {
+            v = n * n * n;
+            s = n * n * 6; 
+        }
+    }
+}
