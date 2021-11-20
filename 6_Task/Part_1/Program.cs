1e0a6e77102cd6654b6368bdb178aac4d7fda8b7
Федор Александров <fedfed95@yandex.ru>
2021-11-20 16:09:39 +0300

    Решение двух задач на 6 занятие

---
+++ 6_Task/Part_1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются.");
+            string line = Console.ReadLine();
+            string[] arr = line.Split();
+
+
+            //Определение длинны самого длинного слова 
+            int lengthLine = 0;
+            foreach (string item in arr)
+            {
+                if (lengthLine < item.Length)
+                {
+                    lengthLine = item.Length;
+                }
+            }
+
+
+            //Запись в список самых длинные слов(их может быть несколько).
+            List<string> words = new List<string>();
+            foreach (string item in arr)
+            {
+                if (lengthLine == item.Length)
+                {
+                    words.Add(item);
+                }
+            }
+
+
+            //Вывод самых длинных слов через запятую, если их несколько. 
+            Console.Write("Вывод самых длинных слов: ");
+            for (int i = 0; i < words.Count(); i++)
+            {
+                Console.Write(words[i].ToString());
+                if (i<words.Count()-1)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6_Task/Part_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются.");
+            string lineRead = Console.ReadLine();
+            string[] arr = lineRead.Split();
+            string line = null;
+            //Запись 
+            foreach (string item in arr)
+            {
+                line += item.ToLower();
+            }
+
+            string lineReverse = null;
+            foreach (string item in arr.Reverse())
+            {
+                string word = null;
+                foreach (char i in item.ToLower().Reverse())
+                {
+                    word += i;
+                }
+                lineReverse += word;  
+            }
+
+            if (line == lineReverse)
+            {
+                Console.WriteLine("Данное пердложение полидром");
+            }
+            else
+            {
+                Console.WriteLine("Данное предложение не является полидромом");
+            }
+
+            Console.WriteLine(line);
+            Console.WriteLine(lineReverse);
+            Console.ReadKey();
+        }
+    }
+}
