6392d9c98f6aabaed0eec5f2a73a29454153ed47
Федор Александров <fedfed95@yandex.ru>
2021-11-22 02:05:07 +0300

    11 Занятие

---
+++ 11_Task/Part_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = 4;
+            double b = 2;
+            Equation eq = new Equation(k, b);
+            Console.WriteLine(eq.Root());
+            Console.ReadKey();
+        }
+    }
+    struct Equation
+    {
+        double K { get; set; }
+        double B { get; set; }
+
+        public Equation(double k, double b)
+        {
+            K = k;
+            B = b;
+        }
+
+        public double Root()
+        {
+            double result = -B / K;
+            return result;
+        }
+    }
+}
