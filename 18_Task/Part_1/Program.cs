96a18255b1dcd59e7aaf3dd3a0bb37ccee7070fe
Федор Александров <fedfed95@yandex.ru>
2021-11-25 23:43:33 +0300

    18 Задание.

--- 18_Task/Part_1/Program.cs
+++ 18_Task/Part_1/Program.cs
@@ -10,6 +10,54 @@ namespace Part_1
     {
         static void Main(string[] args)
         {
+            //string line = "({()})))";
+            string line = "([]{})[]";
+
+            Stack<char> stackSymbols = new Stack<char>();
+
+            try
+            {
+                foreach (char i in line)
+                {
+                    
+                    switch (i)
+                    {
+                        case '(':
+                            stackSymbols.Push(')');
+                            break;
+                        case '{':
+                            stackSymbols.Push('}');
+                            break;
+                        case '[':
+                            stackSymbols.Push(']');
+                            break;
+                        default:
+                            if (stackSymbols.Peek() == i)
+                            {
+                                stackSymbols.Pop();
+                            }
+                            break;
+                    }
+                }
+                Console.WriteLine(line);
+                if (stackSymbols.Count() == 0)
+                {
+                    Console.WriteLine("корректно  расставлены скобки");
+                }
+                else
+                {
+                    Console.Write("НЕкорректно  расставлены скобки");
+                }
+                Console.ReadKey();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(line);
+                Console.WriteLine("Ошибка! " + ex.Message);
+                Console.ReadKey();
+            }
+
         }
     }
 }
