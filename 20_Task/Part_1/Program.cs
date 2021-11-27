49105949bced0c3a29d7d254ab49918dbcf168a9
Федор Александров <fedfed95@yandex.ru>
2021-11-27 19:43:58 +0300

    Исправил.

--- 20_Task/Part_1/Program.cs
+++ 20_Task/Part_1/Program.cs
@@ -12,28 +12,28 @@ namespace Part_1
         {
             Console.WriteLine("Введите радиус окружности:");
             double R = Convert.ToDouble(Console.ReadLine());
+
             Func<double, double> func = new Func<double, double>(CircleLength);
-            func += CircleArea;
-            func += SphereVolume;
-            func?.Invoke(R);
+            Console.WriteLine(" Из метода: " + func?.Method.ToString()+ " Результат: {0:F2}",func?.Invoke(R));
+            func = CircleArea;
+            Console.WriteLine(" Из метода: " + func?.Method.ToString() + " Результат: {0:F2}", func?.Invoke(R));
+            func = SphereVolume;
+            Console.WriteLine(" Из метода: " + func?.Method.ToString() + " Результат: {0:F2}", func?.Invoke(R));
             Console.ReadKey();
         }
         static double CircleLength (double R)
         {
             double D = 2 * Math.PI * R;
-            Console.WriteLine("Длина окружности равна {0:F2}", D);
             return D;
         }
         static double CircleArea (double R)
         {
             double S = Math.PI * Math.Pow(R, 2);
-            Console.WriteLine("Площадь окружности равна {0:F2}", S);
             return S;
         }
         static double SphereVolume (double R)
         {
             double V = 4 / 3 * Math.PI * Math.Pow(R, 3);
-            Console.WriteLine("Объем сферы равен {0:F2}", V);
             return V;
         }
     }
